14c4b345a65d010ea66de251ee22232a0d738f31
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-04-03 15:54:18 -0400

    building out items category function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,19 @@ class ItemsController < ApplicationController
         @categories = Category.all
     end 
 
+    def create
+        # validate for repeated items
+        @item = Item.new(item_params)
+       
+
+
+    end 
+
 
     private
 
-    
+    def item_params
+        params.require(:item).permit(:name, :description, :price, :points, :category_id)
+    end
 
 end
