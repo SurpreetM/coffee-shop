83a53255d9ebbbe8eee5fb2d9a55cba09ead6fdc
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-04-03 15:39:29 -0400

    adding items index and new views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+    end
+
+    def new
+        @item = Item.new
+        @categories = Category.all
+    end 
+
+
+    private
+
+    
+
 end
