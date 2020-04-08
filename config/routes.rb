d6689322d015115670543d9681cff347349386ed
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-04-08 16:15:25 -0400

    adding nested resources for comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   resources :categories
   resources :comments
   resources :purchased_items
-  resources :items
+  resources :items do 
+    resources :comments
+  end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
