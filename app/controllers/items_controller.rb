b8f4df9535e901946c0e280b9a9631481efdaff2
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-04-08 15:01:35 -0400

    Adding detete items path for admins

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,11 @@ class ItemsController < ApplicationController
         end
     end 
 
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+        redirect_to items_path, notice: "You deleted #{@item.name}"
+    end
 
     private
 
