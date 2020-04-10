27b44c2f8a02228fd14c586e4b74c34456860ab6
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-04-10 14:54:23 -0400

    working on adding in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+
+    def index
+        @comments = Comment.all
+    end
+
+    def new
+        @item = Item.find(params[:item_id])
+        @comment = Comment.new
+    end
+
+    def create
+        @item = Item.find(params[:id])
+        @comment = Comment.new(item_id: params[:item_id], decription: comment_params[:decription], rating: comment_params[:rating], user_id: 1)
+        if @comment.save
+            redirect_to comments_path
+        else 
+            @item = Item.find(params[:item_id])
+            render :new
+        end
+    end 
+
+    def comment_params
+        params.require(:comment).permit(:rating, :decription, :user_id, :item_id)
+    end
+
 end
